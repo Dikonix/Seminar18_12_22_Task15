25f5335bedd2ac8fbcb339e4dd353fece48a65ee
Diana Brik <di.brik@yandex.kz>
2022-12-21 13:19:07 +0600

    Seminar 18.12.22 Task 15

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите день недели от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number > 5) && (number < 8))
+{
+    Console.WriteLine("Выходной");
+}
+else if ((number > 0) && (number < 6))
+{
+    Console.WriteLine("Будни");
+}
+else
+{
+    Console.WriteLine("Введено некорректное значение");
+}
